81f0d408ec0296f53766ad421ec8c62a12be08aa
Joel Dubowy <jdubowy@gmail.com>
2014-08-28 17:25:49 -0700

    More WIP on module and script to extract single point pm2.5 time series

--- bin/bluesky/extract_point_pm25_time_series.py
+++ bin/bluesky/extract_point_pm25_time_series.py
@@ -7,6 +7,7 @@ levels at a specific lat/lon from a bluesky dispersion nc output file.
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2014, AirFire, PNW, USFS"
 
+import json
 import os
 import sys
 from optparse import OptionParser
@@ -15,15 +16,14 @@ try:
     from pyairfire.bluesky.dispersionnc import PointExtractor
 except:
     root_dir = os.path.abspath(os.path.join(sys.path[0], '../../'))
-    print root_dir
     sys.path.insert(0, root_dir)
     from pyairfire.bluesky.dispersionnc import PointExtractor
 
 
-def exit_with_msg(msg, extra_output):
-        print msg
-        print
-        extra_output()
+def exit_with_msg(msg, extra_output=None):
+        print "* Error: %s" % (msg)
+        if extra_output:
+            extra_output()
         sys.exit(1)
 
 def parse_options():
@@ -31,18 +31,35 @@ def parse_options():
     parser = OptionParser(usage=usage)
     parser.add_option("-f", "--dispersion-nc-file", dest="nc_file_pathname",
                       help="netCDF input file pathname (required)", metavar="FILE")
+    parser.add_option("--lat", dest="lat", help="latitude (required)", type=float)
+    parser.add_option("--lng", dest="lng", help="longitude (required)", type=float)
+    parser.add_option("-v", "--verbose", dest="verbose", help="longitude (required)", type=float)
 
     options, args = parser.parse_args()
 
     if not options.nc_file_pathname:
-        exit_with_msg("* Error: specify nc filename ('-f')", lambda: parser.print_help())
+        exit_with_msg("specify nc filename ('-f')", lambda: parser.print_help())
+    if not options.lat or not options.lng:
+        exit_with_msg("specify lat and lng ('--lat/--lng')", parser.print_help)
+
+    if options.verbose:
+        print "NC file pathname: %s" % (options.nc_file_pathname)
+        print "latitude: %s" % (options.lat)
+        print "Longitude: %s" % (options.lng)
 
     return options
 
 def main():
     options = parse_options()
 
-    PointExtractor(options.nc_file_pathname)
+    try:
+        pe = PointExtractor(options.nc_file_pathname)
+        point_time_series = pe.extract(options.lat, options.lng)
+
+        print json.dumps([float(e) for e in point_time_series])
+
+    except Exception, e:
+        exit_with_msg(e.message)
 
 if __name__ == "__main__":
     main()
--- pyairfire/bluesky/dispersionnc.py
+++ pyairfire/bluesky/dispersionnc.py
@@ -2,12 +2,16 @@
 
 """dispersionnc.py: Provides uilities for extracting data from smoke
 dispersion nc files.
+
+@see: https://github.com/ecolell/netcdf
 """
 
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2014, AirFire, PNW, USFS"
 
-#from netcdf import netcdf
+import os
+
+from netcdf import netcdf
 
 __all__ = [
     'PointExtractor'
@@ -17,9 +21,66 @@ __all__ = [
 class PointExtractor(object):
 
     def __init__(self, nc_file_pathname):
-        #self.nc_file = netcdf.open(nc_file_pathname)
-        pass
+        if not os.path.isfile(nc_file_pathname):
+            raise RuntimeError("%s doesn't exists" % (nc_file_pathname))
+
+        (self.nc_file, is_new) = netcdf.open(nc_file_pathname)
 
     def extract(self, lat, lng):
-        import pdb;pdb.set_trace()
-        raise NotImplementedError
+        (x,y) = self._compute_grid_indices(lat, lng)
+        data = self.nc_file.getvar('PM25')
+        #data = self.nc_file.obtain_variable('PM25')
+
+        point_time_series = data[0, :, 0, x, y]
+
+        return point_time_series
+
+
+    def _compute_grid_indices(self, lat, lng):
+        # TODO: Compute based on domain corner coordinate, domain resolution,
+        # and lat/lng
+
+        sw_lat, sw_lng = self._sw_corner()
+        lat_res, lng_res = self._resolution()
+
+        lat_index = 0 # compute actual value from sw_lat, lat, and lat_res
+        lng_index = 0 # compute actual value from sw_lng, lng, and lng_res
+
+        return (lat_index, lng_index)
+
+
+    def _sw_corner(self):
+        # TODO: the global attributes ':XORIG' and ':YORIG' seem to define the
+        # SW corner of the domain  For example, the following is from executing 'ncdump -h'
+        # on a smoke_dispersion.nc file:
+        #  ...
+        #  :XCENT = -118.300003051758 ;
+        #  :YCENT = 45. ;
+        #  :XORIG = -128.300003051758 ;
+        #  :YORIG = 40. ;
+        #  ...
+        # read those values
+        #
+        # If this proves impossible, they can be read from summar.json and passed into this
+        # class' contstructor
+
+        yorig = 40.0 # TODO: read actual value
+        xorig = -128.0 # TODO: read actual value
+        return (yorig, xorig)
+
+    def _resolution(self):
+        # TODO: the global attributes ':XCELL' an ':YCELL' seem to define the
+        # resolution.  For example, the following is from executing 'ncdump -h'
+        # on a smoke_dispersion.nc file:
+        #  ...
+        #  :XCELL = 0.0399999991059303 ;
+        #  :YCELL = 0.0399999991059303 ;
+        #  ...
+        # read those values
+        #
+        # If this proves impossible, they can be read from summar.json and passed into this
+        # class' contstructor
+
+        ycell = 0.4 # TODO: read actual value
+        xcell = 0.4 # TODO: read actual value
+        return (ycell, xcell)
