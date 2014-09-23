bdd81c66206e38b78660d2b4fe21996695f630cf
Joel Dubowy <jdubowy@gmail.com>
2014-09-23 13:14:13 -0700

    Pass empty options into parse_requirements for older pip installations

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from pip.req import parse_requirements
 
-REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt', options={})]
 
 setup(
     name='pyairfire',
