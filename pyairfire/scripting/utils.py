8f8c9b8b0fe005cc0bc1046d9968221147c27eba
Joel Dubowy <jdubowy@gmail.com>
2015-01-20 17:05:35 -0800

    Add options to exit_with_msg
    Note that I accidentally included some of these changes
    to exit_with_msg in my previous commit

--- pyairfire/scripting/utils.py
+++ pyairfire/scripting/utils.py
@@ -4,10 +4,14 @@ __copyright__   = "Copyright (c) 2015 AirFire, PNW, USFS"
 import sys
 
 def exit_with_msg(msg, extra_output=None, output=None, exit_code=1,
-        prefix="*** ERROR: "):
+        prefix="*** ERROR: ", extra_preceeding_output=None):
+    """Prints message and exits
+    """
     output = output or sys.stderr.write
+    if extra_preceeding_output:
+        extra_preceeding_output()
     output("\n%s%s\n\n" % (prefix, msg))
     if extra_output:
         extra_output()
     output('\n')
-    sys.exit(1)
+    sys.exit(exit_code)
