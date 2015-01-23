62f59614447837055f97f8848495c023bf678756
Joel Dubowy <jdubowy@gmail.com>
2015-01-23 11:29:17 -0800

    Reduce redundancy by using methods that now exist in in pyairfire.scripting.options

--- bin/statuslogging/log-status.py
+++ bin/statuslogging/log-status.py
@@ -28,77 +28,61 @@ REQUIRED_OPTIONS = [
     {
         'short': '-e',
         'long': '--api-endpoint',
-        'desc': 'api endpoint',
         'dest': 'api_endpoint',
         'help': 'status logger submission API endpoint (required)'
     },
     {
         'short': '-k',
         'long': '--api-key',
-        'desc': 'api key',
         'dest':  'api_key',
         'help': 'api key used to make requests to status logger service (required)'
     },
     {
         'short': '-s',
         'long': '--api-secret',
-        'desc': 'api secret',
         'dest': 'api_secret',
         'help': 'api secret used to make requests to status logger service (required)'
     },
     {
         'short': '-p',
         'long': '--process',
-        'desc': 'process name',
         'dest': 'process',
         'help': 'name of process (required)'
     },
     {
         'short': '-o',
         'long': '--status',
-        'desc': 'status',
         'dest': 'status',
         'help': "String valued status, ex. 'Ok' (required)"
     }
 ]
-
-def parse_options():
-    #usage = "usage: %prog [options]"
-    parser = OptionParser() #usage=usage)
-
-    # Required options
-    for ro in REQUIRED_OPTIONS:
-        parser.add_option(ro['short'], ro['long'], dest=ro['dest'], help=ro['help'])
-
-    # Optional
-    parser.add_option('-f', '--field', dest="fields", type="string",
-        action="callback", help="extra fields to add to status log", default={},
-        callback=scripting.options.extract_and_set_key_value)
-    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
-        help="to turn on extra output", default=False)
-
-    options, args = parser.parse_args()
-
-    scripting.options.check_required_options(options, REQUIRED_OPTIONS,
-        lambda: parser.print_help())
-
-    if options.verbose:
-        for ro in REQUIRED_OPTIONS:
-            print "%s (%s'%s'): %s" % (ro['desc'],
-                "'%s', "  % (ro['short']) if ro['short'] else '', ro['long'],
-                options.__dict__[ro['dest']])
-        if options.fields:
-            print "Extra fields ('-f', '--field'):"
-            for k,v  in options.fields.items():
-                print "  %s: %s" % (k, v)
-
-    return options
+OPTIONAL_OPTIONS = [
+    {
+        'short': '-f',
+        'long': '--field',
+        'dest': "fields",
+        'type': "string",
+        'action': "callback",
+        'help': "extra fields to add to status log",
+        'default': {},
+        'callback': scripting.options.extract_and_set_key_value
+    },
+    {
+        'short': "-v",
+        'long': "--verbose",
+        'dest': "verbose",
+        'action': "store_true",
+        'help': "to turn on extra output",
+        'default': False
+    }
+]
 
 def error_handler(e):
     scripting.utils.exit_with_msg("Failed to submit status: %s" % (e))
 
 def main():
-    options = parse_options()
+    parser, options, args = scripting.options.parse_options(REQUIRED_OPTIONS,
+        OPTIONAL_OPTIONS)
 
     try:
         sl = statuslogging.StatusLogger(options.api_endpoint, options.api_key, options.api_secret, options.process)
