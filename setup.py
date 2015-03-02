8aa1b2341f4b1f6cd10288f9f291247a438138f9
Joel Dubowy <jdubowy@gmail.com>
2015-03-02 15:57:28 -0800

    Switch from distutils to setuputils and add various kwargs to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 # Note: using pip.req.parse_requirements like so:
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
@@ -6,29 +6,32 @@ from distutils.core import setup
 #    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
 with open('requirements.txt') as f:
     REQUIREMENTS = f.read().splitlines()
+with open('requirements-test.txt') as f:
+    TEST_REQUIREMENTS = f.read().splitlines()
 
 setup(
     name='pyairfire',
     version='0.6.10',
+    license='MIT',
     author='Joel Dubowy',
     author_email='jdubowy@gmail.com',
-    packages=[
-        'pyairfire',
-        'pyairfire.applogging',
-        'pyairfire.bluesky',
-        'pyairfire.datetime',
-        'pyairfire.fabric',
-        'pyairfire.scripting',
-        'pyairfire.statuslogging',
-        'pyairfire.web'
-    ],
+    packages=find_packages(),
     scripts=[
         'bin/bluesky/extract-point-pm25-time-series.py',
         'bin/bluesky/pre-process-fires',
         'bin/statuslogging/log-status.py',
         'bin/statuslogging/read-status-log'
     ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS"
+    ]
     url='https://github.com/pnwairfire/pyairfire',
     description='General toolbox of python utilities for AirFire team.',
     install_requires=REQUIREMENTS,
+    extras_require=TEST_REQUIREMENTS
 )
