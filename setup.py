971da4748ed531d79068419bc22345badd0ff869
Joel Dubowy <jdubowy@gmail.com>
2015-03-02 17:04:19 -0800

    Update requirements parsing

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
+import re
 from setuptools import setup, find_packages
 
 # Note: using pip.req.parse_requirements like so:
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 # results in the folloing error on Heroku:
 #    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
-with open('requirements.txt') as f:
-    REQUIREMENTS = f.read().splitlines()
-with open('requirements-test.txt') as f:
-    TEST_REQUIREMENTS = f.read().splitlines()
+git_url_matcher = re.compile('^git\+.+/([^/.]+)(\.git)?@v?([0-9.]+)$')
+def parse_requirements(dep_links, req_file_name):
+    with open(req_file_name) as f:
+        reqs = []
+        for r in f.read().splitlines():
+            m = git_url_matcher.match(r)
+            if m:
+                dep_links.append(r)
+                reqs.append("%s==%s" % (m.group(1), m.group(3)))
+            else:
+                reqs.append(r)
+        return reqs
+
+dependency_links = []
+requirements = parse_requirements(dependency_links, 'requirements.txt')
+test_requirements = parse_requirements(dependency_links, 'requirements-test.txt')
 
 setup(
     name='pyairfire',
@@ -32,6 +45,7 @@ setup(
     ],
     url='https://github.com/pnwairfire/pyairfire',
     description='General toolbox of python utilities for AirFire team.',
-    install_requires=REQUIREMENTS,
-    tests_require=TEST_REQUIREMENTS
+    install_requires=requirements,
+    dependency_links=dependency_links,
+    tests_require=test_requirements
 )
