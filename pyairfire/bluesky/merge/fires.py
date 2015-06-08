11a538ba072443bad9bf91ce6bed00a7b92f775b
Joel Dubowy <jdubowy@gmail.com>
2015-06-08 10:39:25 -0700

    Move fire merging code into module for reuse
    This means that pre-process-fires is no longer self contained,
    but it's worth the trade-off

---
+++ pyairfire/bluesky/merge/fires.py
@@ -0,0 +1,60 @@
+"""pyairfire.bluesky.merge.fires
+
+TODO: write unit tests (using the example in the usage text of
+bin/pre-process-fires for one test case).
+"""
+
+__author__      = "Joel Dubowy"
+__copyright__   = "Copyright (c) 2015 AirFire, PNW, USFS"
+
+import csv
+import sys
+
+class FiresMerger(object):
+
+    class FireFile(object):
+        def __init__(self, file_specifier):
+            a = file_specifier.split(':')
+            if len(a) > 2:
+                raise RuntimeError("Invalid fire file specifier: %s" % (file_specifier))
+            self.file_name = a[0]
+            if len(a) == 2:
+                if not a[1]:
+                    raise RuntimeError("Invalid fire file specifier: %s" % (file_specifier))
+                self.country_code_whitelist = set(a[1].split(','))
+            else:
+                self.country_code_whitelist = None
+
+    def __init__(self, *fire_files):
+        self._fire_files = [FiresMerger.FireFile(f) for f in fire_files]
+        self._merge()
+
+    def _merge(self):
+        self._headers = None
+        self._fires = reduce(lambda a,b: a+b,
+            [self._process_file(f) for f in self._fire_files])
+
+    def _process_file(self, f):
+        rows = []
+        with open(f.file_name, 'r') as input_file:
+            headers = []
+            for row in csv.reader(input_file):
+                if not headers:
+                    headers = [e.strip(' ') for e in row]
+                    if not self._headers:
+                        self._headers = headers
+                    else:
+                        self._headers.extend(sorted(set(headers).difference(self._headers)))
+                    headers = dict([(headers[i], i) for i in xrange(len(headers))])
+                else:
+                    if (not f.country_code_whitelist or
+                        row[headers['country']] in f.country_code_whitelist):
+                        rows.append({h:row[headers[h]] for h in headers})
+        return rows
+
+    def write(self, output_file=None):
+        stream = open(output_file, 'w') if output_file else sys.stdout
+        csvfile = csv.writer(stream, lineterminator='\n')
+        csvfile.writerow(self._headers)
+        for f in self._fires:
+            csvfile.writerow([f.get(h, '') for h in self._headers])
