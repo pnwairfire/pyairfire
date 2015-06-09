ea251ba8e0bfd639fdb7459a80fdb22489560d63
Joel Dubowy <jdubowy@gmail.com>
2015-06-08 17:42:17 -0700

    More WIP on emissions merge script

--- pyairfire/bluesky/merge/emissions.py
+++ pyairfire/bluesky/merge/emissions.py
@@ -18,14 +18,14 @@ class EmissionsMerger(FiresMerger):
 
     class FileSet(object):
         def __init__(self, file_set_specifier):
-            a = file_set_specifier.split(';'):
-            if len(a) != 2:
-                raise RuntimeError("Invalid fire file specifier: %s" % (file_specifier))
+            a = file_set_specifier.split(':')
+            if len(a) not in (2,3):
+                raise RuntimeError("Invalid file set specifier: %s" % (file_set_specifier))
             self.emissions_file = EmissionsMerger.FileSpecifier(a[0])
-            self.fire_file = EmissionsMerger.FileSpecifier(a[1])
+            self.fire_file = EmissionsMerger.FileSpecifier(':'.join(a[1:]))
 
     def __init__(self, *file_sets):
-        self._file_sets = [(EmissionsMerger.FileSet(fs) for fs in file_sets]
+        self._file_sets = [EmissionsMerger.FileSet(fs) for fs in file_sets]
         self._merge()
 
     def _merge(self):
@@ -38,12 +38,12 @@ class EmissionsMerger(FiresMerger):
         for file_set in self._file_sets:
             fires = self._process_file(file_set.fire_file)
             emissions = self._process_file(file_set.emissions_file,
-                set([fire['id'] for fire in fires])
+                set([fire['id'] for fire in fires]))
             self._fires.extend(fires)
             self._emissions.extend(emissions)
 
-    def write(self, fire_locations_file, emissions_file):
-        FiresMerge(self).write(fire_locations_file)
+    def write(self, emissions_file, fire_locations_file):
+        FiresMerger(self).write(fire_locations_file)
 
         stream = open(emissions_file, 'w')
         csvfile = csv.writer(stream, lineterminator='\n')
