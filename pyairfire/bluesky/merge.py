944481f75f75b2784a9a147803692850a76ad079
Joel Dubowy <jdubowy@gmail.com>
2015-06-15 15:44:40 -0700

    Update merge-emissions to merge events files as well
    So, in all, it merges the emissions, events, and locations files

--- pyairfire/bluesky/merge.py
+++ pyairfire/bluesky/merge.py
@@ -84,11 +84,12 @@ class EmissionsMerger(MergerBase):
     class FileSet(object):
         def __init__(self, file_set_specifier):
             a = file_set_specifier.split(':')
-            if len(a) not in (2,3):
+            if len(a) not in (3,4):
                 raise RuntimeError("Invalid file set specifier: %s" % (
                     file_set_specifier))
             self.emissions_file = EmissionsMerger.FileSpecifier(a[0])
-            self.fire_file = EmissionsMerger.FileSpecifier(':'.join(a[1:]))
+            self.events_file = EmissionsMerger.FileSpecifier(a[1])
+            self.fire_file = EmissionsMerger.FileSpecifier(':'.join(a[2:]))
 
     def __init__(self, *file_sets, **options):
         self._file_sets = [EmissionsMerger.FileSet(fs) for fs in file_sets]
@@ -100,22 +101,35 @@ class EmissionsMerger(MergerBase):
         """
         self._fire_headers = set()
         self._emissions_headers = set()
+        self._events_headers = set()
         self._fires = []
         self._emissions = []
+        self._events = []
         for file_set in self._file_sets:
             fires = self._process_file(file_set.fire_file)
             if fires:
                 self._fires.extend(fires)
                 self._fire_headers |= set(fires[0].keys())
+                # load emissions
                 fire_ids = set([fire['id'] for fire in fires])
                 emissions = self._process_file(file_set.emissions_file,
                     lambda row: row.get(self._fire_id_key) in fire_ids)
                 if emissions:
                     self._emissions.extend(emissions)
                     self._emissions_headers |= set(emissions[0].keys())
+                # load events
+                event_ids = set([fire['event_id'] for fire in fires])
+                events = self._process_file(file_set.events_file,
+                    lambda row: row.get('id') in event_ids)
+                if events:
+                    self._events.extend(events)
+                    self._events_headers |= set(events[0].keys())
 
-    def write(self, emissions_file, fire_locations_file):
+
+    def write(self, emissions_file, events_file, fire_locations_file):
         f_stream = open(fire_locations_file, 'w')
         self._write(f_stream, self._fire_headers, self._fires)
         e_stream = open(emissions_file, 'w')
         self._write(e_stream, self._emissions_headers, self._emissions)
+        v_stream = open(events_file, 'w')
+        self._write(v_stream, self._events_headers, self._events)
--- test/pyairfire/bluesky/merge_test.py
+++ test/pyairfire/bluesky/merge_test.py
@@ -17,10 +17,17 @@ except:
 
 
 FIRE_LOCATIONS_1 = """\
-id,name,lat,lng,date_time,foo,country
-12ho123,Fire A,47.12,-118.34,201405310000Z,foo,USA
-dsdho123,Fire B,47.22,-118.3423,201405310000Z,foofoo,USA
-sldj2343,Fire C,47.22,-118.3423,201405310000Z,oof,MX
+id,event_id,name,lat,lng,date_time,foo,country
+12ho123,aa,Fire A,47.12,-118.34,201405310000Z,foo,USA
+dsdho123,bb,Fire B,47.22,-118.3423,201405310000Z,foofoo,USA
+sldj2343,cc,Fire C,47.22,-118.3423,201405310000Z,oof,MX
+"""
+EVENTS_1 = """\
+id,event_name,total_area
+aa,nameaa,12
+bb,namebb,43
+cc,namecc,343
+dd,namedd,i3o4
 """
 EMISSIONS_1 = """\
 fire_id, foo, bar
@@ -34,10 +41,17 @@ sdfdsf,foo34,bar3423
 """
 
 FIRE_LOCATIONS_2 = """\
-id,name,lat,lng,date_time,baz,bar,country
-12ho123,Fire ZZ,50.12,-112.34,201405310000Z,baz,bar,USA
-sldj2343,Fire YY,42.22,-109.3423,201405310000Z,zab,rab,MX
-dsdho123,Fire XX,37.22,-112.3423,201405310000Z,bazbaz,barbar,CA
+id,event_id,name,lat,lng,date_time,baz,bar,country
+12ho123,xx,Fire ZZ,50.12,-112.34,201405310000Z,baz,bar,USA
+sldj2343,yy,Fire YY,42.22,-109.3423,201405310000Z,zab,rab,MX
+dsdho123,zz,Fire XX,37.22,-112.3423,201405310000Z,bazbaz,barbar,CA
+"""
+EVENTS_2 = """\
+id,event_name,eventfoo
+aa,nameaa,dsf
+zz,namezz,sdfsdf
+xx,namexx,sdfr
+yy,nameyy,rkhw
 """
 EMISSIONS_2 = """\
 fire_id, foo, baz
@@ -49,8 +63,12 @@ sk3r3l4k,foo345,baz4433
 """
 
 FIRE_LOCATIONS_3 = """\
-id,name,lat,lng,date_time,bar,baz,aaa,country
-sdfdsf,Fire L,44.22,-116.342,201405310000Z,bary,bazy,aaaaa,CA
+id,event_id,name,lat,lng,date_time,bar,baz,aaa,country
+sdfdsf,ff,Fire L,44.22,-116.342,201405310000Z,bary,bazy,aaaaa,CA
+"""
+EVENTS_3 = """\
+id,event_name
+ff,ksdf
 """
 EMISSIONS_3 = """\
 sldj2343,foo34,baz43342
@@ -58,14 +76,24 @@ sk3r3l4k,foo345,baz4433
 """
 
 MERGED_ALL_FIRE_LOCATIONS = """\
-aaa,bar,baz,country,date_time,foo,id,lat,lng,name
-,,,USA,201405310000Z,foo,12ho123,47.12,-118.34,Fire A
-,,,USA,201405310000Z,foofoo,dsdho123,47.22,-118.3423,Fire B
-,,,MX,201405310000Z,oof,sldj2343,47.22,-118.3423,Fire C
-,bar,baz,USA,201405310000Z,,12ho123,50.12,-112.34,Fire ZZ
-,rab,zab,MX,201405310000Z,,sldj2343,42.22,-109.3423,Fire YY
-,barbar,bazbaz,CA,201405310000Z,,dsdho123,37.22,-112.3423,Fire XX
-aaaaa,bary,bazy,CA,201405310000Z,,sdfdsf,44.22,-116.342,Fire L
+aaa,bar,baz,country,date_time,event_id,foo,id,lat,lng,name
+,,,USA,201405310000Z,aa,foo,12ho123,47.12,-118.34,Fire A
+,,,USA,201405310000Z,bb,foofoo,dsdho123,47.22,-118.3423,Fire B
+,,,MX,201405310000Z,cc,oof,sldj2343,47.22,-118.3423,Fire C
+,bar,baz,USA,201405310000Z,xx,,12ho123,50.12,-112.34,Fire ZZ
+,rab,zab,MX,201405310000Z,yy,,sldj2343,42.22,-109.3423,Fire YY
+,barbar,bazbaz,CA,201405310000Z,zz,,dsdho123,37.22,-112.3423,Fire XX
+aaaaa,bary,bazy,CA,201405310000Z,ff,,sdfdsf,44.22,-116.342,Fire L
+"""
+MERGED_EVENTS_ALL_FIRE_LOCATIONS = """\
+event_name,eventfoo,id,total_area
+nameaa,,aa,12
+namebb,,bb,43
+namecc,,cc,343
+namezz,sdfsdf,zz,
+namexx,sdfr,xx,
+nameyy,rkhw,yy,
+ksdf,,ff,
 """
 MERGED_EMISSIONS_ALL_FIRE_LOCATIONS = """\
 bar,baz,fire_id,foo
@@ -80,11 +108,18 @@ bar46,,12ho123,foo454
 """
 
 MERGED_ALL_FIRE_LOCATIONS_CA_MX_ONLY = """\
-aaa,bar,baz,country,date_time,foo,id,lat,lng,name
-,,,MX,201405310000Z,oof,sldj2343,47.22,-118.3423,Fire C
-,rab,zab,MX,201405310000Z,,sldj2343,42.22,-109.3423,Fire YY
-,barbar,bazbaz,CA,201405310000Z,,dsdho123,37.22,-112.3423,Fire XX
-aaaaa,bary,bazy,CA,201405310000Z,,sdfdsf,44.22,-116.342,Fire L
+aaa,bar,baz,country,date_time,event_id,foo,id,lat,lng,name
+,,,MX,201405310000Z,cc,oof,sldj2343,47.22,-118.3423,Fire C
+,rab,zab,MX,201405310000Z,yy,,sldj2343,42.22,-109.3423,Fire YY
+,barbar,bazbaz,CA,201405310000Z,zz,,dsdho123,37.22,-112.3423,Fire XX
+aaaaa,bary,bazy,CA,201405310000Z,ff,,sdfdsf,44.22,-116.342,Fire L
+"""
+MERGED_EVENTS_ALL_FIRE_LOCATIONS_CA_MX_ONLY = """\
+event_name,eventfoo,id,total_area
+namecc,,cc,343
+namezz,sdfsdf,zz,
+nameyy,rkhw,yy,
+ksdf,,ff,
 """
 MERGED_EMISSIONS_ALL_FIRE_LOCATIONS_CA_MX_ONLY = """\
 bar,baz,fire_id,foo
@@ -95,10 +130,16 @@ bar3,,sldj2343,foo545
 """
 
 MERGED_1_FIRE_LOCATION = """\
-country,date_time,foo,id,lat,lng,name
-USA,201405310000Z,foo,12ho123,47.12,-118.34,Fire A
-USA,201405310000Z,foofoo,dsdho123,47.22,-118.3423,Fire B
-MX,201405310000Z,oof,sldj2343,47.22,-118.3423,Fire C
+country,date_time,event_id,foo,id,lat,lng,name
+USA,201405310000Z,aa,foo,12ho123,47.12,-118.34,Fire A
+USA,201405310000Z,bb,foofoo,dsdho123,47.22,-118.3423,Fire B
+MX,201405310000Z,cc,oof,sldj2343,47.22,-118.3423,Fire C
+"""
+MERGED_EVENTS_1_FIRE_LOCATION = """\
+event_name,id,total_area
+nameaa,aa,12
+namebb,bb,43
+namecc,cc,343
 """
 MERGED_EMISSIONS_1_FIRE_LOCATION = """\
 bar,fire_id,foo
@@ -110,8 +151,12 @@ bar46,12ho123,foo454
 """
 
 MERGED_1_FIRE_LOCATION_CA_MX_ONLY = """\
-country,date_time,foo,id,lat,lng,name
-MX,201405310000Z,oof,sldj2343,47.22,-118.3423,Fire C
+country,date_time,event_id,foo,id,lat,lng,name
+MX,201405310000Z,cc,oof,sldj2343,47.22,-118.3423,Fire C
+"""
+MERGED_EVENTS_1_FIRE_LOCATION_CA_MX_ONLY = """\
+event_name,id,total_area
+namecc,cc,343
 """
 MERGED_EMISSIONS_1_FIRE_LOCATION_CA_MX_ONLY = """\
 bar,fire_id,foo
@@ -176,65 +221,81 @@ class TestEmissionsMerger(MergerTestBase):
     def test_merge_three(self, tmpdir, monkeypatch):
         fl1 = self._file(tmpdir, "fire_locations_1.csv", FIRE_LOCATIONS_1)
         e1 = self._file(tmpdir, "emissions_1.csv", EMISSIONS_1)
+        v1 = self._file(tmpdir, "events_1.csv", EVENTS_1)
         fl2 = self._file(tmpdir, "fire_locations_2.csv", FIRE_LOCATIONS_2)
         e2 = self._file(tmpdir, "emissions_2.csv", EMISSIONS_2)
+        v2 = self._file(tmpdir, "events_2.csv", EVENTS_2)
         fl3 = self._file(tmpdir, "fire_locations_3.csv", FIRE_LOCATIONS_3)
         e3 = self._file(tmpdir, "emissions_3.csv", EMISSIONS_3)
+        v3 = self._file(tmpdir, "events_3.csv", EVENTS_3)
         merger = merge.EmissionsMerger(
-            "{}:{}".format(e1,fl1),
-            "{}:{}".format(e2,fl2),
-            "{}:{}".format(e3,fl3)
+            "{}:{}:{}".format(e1,v1,fl1),
+            "{}:{}:{}".format(e2,v2,fl2),
+            "{}:{}:{}".format(e3,v3,fl3)
         )
         o_f = tmpdir.join('merged_fires.csv')
         o_e = tmpdir.join('merged_emissions.csv')
+        o_v = tmpdir.join('merged_events.csv')
         #monkeypatch.setattr(sys.stdout, 'write', s.write)
-        merger.write(o_e.strpath, o_f.strpath)
+        merger.write(o_e.strpath, o_v.strpath, o_f.strpath)
         assert MERGED_EMISSIONS_ALL_FIRE_LOCATIONS  == o_e.read()
+        assert MERGED_EVENTS_ALL_FIRE_LOCATIONS  == o_v.read()
         assert MERGED_ALL_FIRE_LOCATIONS == o_f.read()
 
     def test_merge_three_with_country_code_whitelist(self, tmpdir, monkeypatch):
         fl1 = self._file(tmpdir, "fire_locations_1.csv", FIRE_LOCATIONS_1)
         e1 = self._file(tmpdir, "emissions_1.csv", EMISSIONS_1)
+        v1 = self._file(tmpdir, "events_1.csv", EVENTS_1)
         fl2 = self._file(tmpdir, "fire_locations_2.csv", FIRE_LOCATIONS_2)
         e2 = self._file(tmpdir, "emissions_2.csv", EMISSIONS_2)
+        v2 = self._file(tmpdir, "events_2.csv", EVENTS_2)
         fl3 = self._file(tmpdir, "fire_locations_3.csv", FIRE_LOCATIONS_3)
         e3 = self._file(tmpdir, "emissions_3.csv", EMISSIONS_3)
+        v3 = self._file(tmpdir, "events_3.csv", EVENTS_3)
         merger = merge.EmissionsMerger(
-            "{}:{}:CA,MX".format(e1,fl1),
-            "{}:{}:CA,MX".format(e2,fl2),
-            "{}:{}:CA,MX".format(e3,fl3)
+            "{}:{}:{}:CA,MX".format(e1,v1,fl1),
+            "{}:{}:{}:CA,MX".format(e2,v2,fl2),
+            "{}:{}:{}:CA,MX".format(e3,v3,fl3)
         )
         o_f = tmpdir.join('merged_fires.csv')
         o_e = tmpdir.join('merged_emissions.csv')
+        o_v = tmpdir.join('merged_events.csv')
         #monkeypatch.setattr(sys.stdout, 'write', s.write)
-        merger.write(o_e.strpath, o_f.strpath)
+        merger.write(o_e.strpath, o_v.strpath, o_f.strpath)
         assert MERGED_EMISSIONS_ALL_FIRE_LOCATIONS_CA_MX_ONLY  == o_e.read()
+        assert MERGED_EVENTS_ALL_FIRE_LOCATIONS_CA_MX_ONLY  == o_v.read()
         assert MERGED_ALL_FIRE_LOCATIONS_CA_MX_ONLY == o_f.read()
 
     def test_merge_one(self, tmpdir, monkeypatch):
         fl1 = self._file(tmpdir, "fire_locations_1.csv", FIRE_LOCATIONS_1)
         e1 = self._file(tmpdir, "emissions_1.csv", EMISSIONS_1)
+        v1 = self._file(tmpdir, "events_1.csv", EVENTS_1)
         merger = merge.EmissionsMerger(
-            "{}:{}".format(e1,fl1)
+            "{}:{}:{}".format(e1,v1,fl1)
         )
         o_f = tmpdir.join('merged_fires.csv')
         o_e = tmpdir.join('merged_emissions.csv')
+        o_v = tmpdir.join('merged_events.csv')
         #monkeypatch.setattr(sys.stdout, 'write', s.write)
-        merger.write(o_e.strpath, o_f.strpath)
+        merger.write(o_e.strpath, o_v.strpath, o_f.strpath)
         assert MERGED_EMISSIONS_1_FIRE_LOCATION  == o_e.read()
+        assert MERGED_EVENTS_1_FIRE_LOCATION  == o_v.read()
         assert MERGED_1_FIRE_LOCATION == o_f.read()
 
     def test_merge_one_with_country_code_whitelist(self, tmpdir, monkeypatch):
         fl1 = self._file(tmpdir, "fire_locations_1.csv", FIRE_LOCATIONS_1)
         e1 = self._file(tmpdir, "emissions_1.csv", EMISSIONS_1)
+        v1 = self._file(tmpdir, "events_1.csv", EVENTS_1)
         merger = merge.EmissionsMerger(
-            "{}:{}:CA,MX".format(e1,fl1)
+            "{}:{}:{}:CA,MX".format(e1,v1,fl1)
         )
         o_f = tmpdir.join('merged_fires.csv')
         o_e = tmpdir.join('merged_emissions.csv')
+        o_v = tmpdir.join('merged_events.csv')
         #monkeypatch.setattr(sys.stdout, 'write', s.write)
-        merger.write(o_e.strpath, o_f.strpath)
+        merger.write(o_e.strpath, o_v.strpath, o_f.strpath)
         assert MERGED_EMISSIONS_1_FIRE_LOCATION_CA_MX_ONLY  == o_e.read()
+        assert MERGED_EVENTS_1_FIRE_LOCATION_CA_MX_ONLY  == o_v.read()
         assert MERGED_1_FIRE_LOCATION_CA_MX_ONLY == o_f.read()
 
 
