37c1f42bbd05c096410374957feb78b0ccf37c9c
Joel Dubowy <jdubowy@gmail.com>
2015-06-26 13:20:26 -0700

    Use fab's confirm method and add color to output

--- pyairfire/fabric/output.py
+++ pyairfire/fabric/output.py
@@ -7,23 +7,30 @@ __copyright__   = "Copyright (c) 2015 AirFire, PNW, USFS"
 import os
 import sys
 
+from fabric.colors import red, green, yellow
+from fabric.contrib.console import confirm as fab_confirm
+
 __all__ = [
     'error',
     'debug_log'
 ]
 
 def error(msg):
-    print "*** ERROR:  %s" % (msg)
+    print(red("*** ERROR:  %s" % (msg)))
     sys.exit(1)
 
 def debug_log(msg):
     if os.environ.has_key('DEBUG'):
-        print msg
+        print(green(msg))
 
 def confirm(msg):
+    """Displays message and then prompts user to continue.
+
+    Exits unless user responds with '(y)es'.
+    """
+    #  See http://docs.fabfile.org/en/1.10/api/contrib/console.html
     print(msg)
-    sys.stdout.write("Do you want to continue (y/n)?: ")
-    r = sys.stdin.readline().strip()
-    if 'y' != r.lower():
+    r = fab_confirm("Do you want to continue?", default=False)
+    if not r:
         print("Exiting.")
         sys.exit(0)
