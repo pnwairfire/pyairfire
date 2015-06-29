c14c69c53d207d3c27bd0f8d13f17374ce14cc13
Joel Dubowy <jdubowy@gmail.com>
2015-06-29 16:31:19 -0700

    bump version to 0.8.14

--- pyairfire/__init__.py
+++ pyairfire/__init__.py
@@ -1,5 +1,5 @@
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright (c) 2015 AirFire, PNW, USFS"
 
-__version_info__ = (0,8,13)
+__version_info__ = (0,8,14)
 __version__ = '.'.join([str(n) for n in __version_info__])
