1fb27e321ed4bc56daa935707519063ff9261fe4
Joel Dubowy <jdubowy@gmail.com>
2015-11-19 13:54:15 -0800

    Fill in email logic in hipchat archiver

--- pyairfire/hipchat/archive.py
+++ pyairfire/hipchat/archive.py
@@ -10,8 +10,12 @@ import logging
 import os
 import re
 import requests
+import smtplib
 import StringIO
 import zipfile
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from pyairfire.scripting.utils import exit_with_msg
 
@@ -22,6 +26,11 @@ __all__ = [
 class HipChatArchiver(object):
     ARCHIVE_DATETIME_FORMAT = "%Y%m%dT%H%S%M"
 
+    # TODO: should there indeed be a default sender?
+    DEFAULT_EMAIL_SENDER = "hipchat-archiver@airfire.org"
+    DEFAULT_EMAIL_SUBJECT = "HipChat archive {} through {}"
+    DEFAULT_SMTP_SERVER = "localhost:25"
+
     def __init__(self, auth_token, num_days, **options):
         self._auth_token = auth_token
         self._start_date = datetime.datetime.utcnow()
@@ -30,10 +39,21 @@ class HipChatArchiver(object):
         self._email_recipients = options.get('email_recipients')
         if not self._dest_dir and not self._email_recipients:
             exit_with_msg("Specify either dest_dir or email_recipients, or both.")
-        self._archive_name = "hipchat-archive-{}-{}".format(
-            self._end_date.strftime(self.ARCHIVE_DATETIME_FORMAT),
-            self._start_date.strftime(self.ARCHIVE_DATETIME_FORMAT))
+
+        start_date_str = self._start_date.strftime(self.ARCHIVE_DATETIME_FORMAT)
+        end_date_str =  self._end_date.strftime(self.ARCHIVE_DATETIME_FORMAT)
+
+        self._archive_name = "hipchat-archive-{}-{}".format(end_date_str,
+            start_date_str)
         self._zip_file_name = '{}.zip'.format(self._archive_name)
+
+        self._email_sender = options.get('email_sender') or self.DEFAULT_EMAIL_SENDER
+        self._email_subject = (options.get('email_subject') or
+            self.DEFAULT_EMAIL_SUBJECT.format(start_date_str, end_date_str))
+        self._smtp_server = options.get('smtp_server') or self.DEFAULT_SMTP_SERVER
+        self._smtp_username = options.get('smtp_username')
+        self._smtp_password = options.get('smtp_password')
+        self._smtp_starttls = options.get('smtp_starttls')
 
     def archive(self, room_id=None):
         rooms = self._get_rooms(room_id=room_id)
@@ -123,11 +143,41 @@ class HipChatArchiver(object):
             zf.write(in_memory_zip.read())
 
     def _email(self, in_memory_zip):
-        if self._email_recipients:
+        if not self._email_recipients:
             return
 
         logging.info("Emailing zip file {} to {}".format(
             self._zip_file_name, ', '.join(self._email_recipients)))
+        try:
+            msg = MIMEMultipart('alternative')
+            msg['From'] = self._email_sender
+            msg['To'] = ', '.join(self._email_recipients)
+            msg['Subject'] = self._email_subject
 
-        # TODO: email; handle exceptions
 
+            msg.attach(MIMEText(self._email_subject))
+
+            in_memory_zip.seek(0)
+            msg.attach(MIMEApplication(in_memory_zip.read(),
+                Content_Disposition='attachment; filename="{}"'.format(self._zip_file_name),
+                Name=self._zip_file_name))
+
+            logging.debug('Connecting to SMTP server %s', self._smtp_server)
+            s = smtplib.SMTP(self._smtp_server)
+
+            if self._smtp_starttls:
+                logging.debug('Using STARTTLS')
+                s.ehlo()
+                s.starttls()
+                s.ehlo()
+
+            if self._smtp_username and self._smtp_password:
+                logging.debug('Logging into SMTP server with u/p')
+                s.login(self._smtp_username, self._smtp_password)
+
+            s.sendmail(msg['from'], self._email_recipients, msg.as_string())
+            s.quit()
+
+        except Exception, e:
+            exit_with_msg('Failed to send email to {} - {}'.format(
+                ', '.join(self._email_recipients), e.message))
