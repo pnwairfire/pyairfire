9ac21e63cd0c8132c74a06e0238bfdf5673493f9
Joel Dubowy <jdubowy@gmail.com>
2016-10-18 10:34:23 -0700

    refactor working dir context handler to support specifying dir

--- pyairfire/osutils.py
+++ pyairfire/osutils.py
@@ -8,9 +8,16 @@ import os
 import tempfile
 
 class create_working_dir(object):
+
+    def __init__(self, working_dir=None):
+        self._working_dir = working_dir
+
     def __enter__(self):
         self._original_dir = os.getcwd()
-        self._working_dir = tempfile.mkdtemp()
+        if self._working_dir:
+            os.makedirs(self._working_dir, exist_ok=True)
+        else:
+            self._working_dir = tempfile.mkdtemp()
         logging.debug('chdir to working directory %s', self._working_dir)
         os.chdir(self._working_dir)
         return self._working_dir
@@ -18,4 +25,6 @@ class create_working_dir(object):
     def __exit__(self, type, value, traceback):
         logging.debug('chdir back to original directory %s', self._original_dir)
         os.chdir(self._original_dir)
-        # TODO: delete self._working_dir or just let os clean it up ?
+        # TODO:if it was a temp dir, delete self._working_dir or just let os
+        #   clean it up?  If delete, we'd need to refactor to keep track of
+        #   whether self._working_dir was passed in or not
